05f1b826db797efbc8be91659439631fbedd946c
Mariana Chaves <79344040+marichaves@users.noreply.github.com>
2023-11-15 17:17:09 -0300

    update

--- src/main/java/com/algaworks/erp/model/Empresa.java
+++ src/main/java/com/algaworks/erp/model/Empresa.java
@@ -1,8 +1,8 @@
 package com.algaworks.erp.model;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
-import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -17,33 +17,48 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-@Table
-@Entity(name = "empresa")
+@Entity
+@Table(name = "empresa")
 public class Empresa implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(name = "nome_fantasia")
+	@Column(name = "nome_fantasia", nullable = false, length = 80)
 	private String nomeFantasia;
 	
-	@Column(name = "razao_social")
+	@Column(name = "razao_social", nullable = false, length = 120)
 	private String razaoSocial;
 	
+	@Column(nullable = false, length = 18)
 	private String cnpj;
 	
-	@Temporal(TemporalType.TIMESTAMP)
+	@Temporal(TemporalType.DATE)
+	@Column(name = "data_fundacao")
 	private Date dataFundacao;
 	
 	@ManyToOne
-	@JoinColumn(name = "ramo_atividade_id")
+	@JoinColumn(name = "ramo_atividade_id", nullable = false)
 	private RamoAtividade ramoAtividade;
 	
 	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 30)
 	private TipoEmpresa tipo;
+	
+	@Column(precision = 10, scale = 2)
+	private BigDecimal faturamento;
+	
+	public BigDecimal getFaturamento() {
+        return faturamento;
+    }
+	
+	public void setFaturamento(BigDecimal faturamento) {
+        this.faturamento = faturamento;
+    }
+
 	public Long getId() {
 		return id;
 	}
@@ -92,9 +107,20 @@ public class Empresa implements Serializable {
 		this.ramoAtividade = ramoAtividade;
 	}
 
+	public TipoEmpresa getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoEmpresa tipo) {
+		this.tipo = tipo;
+	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -106,12 +132,16 @@ public class Empresa implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Empresa other = (Empresa) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 	@Override
 	public String toString() {
 		return "Empresa [id=" + id + "]";
-	}
-	
-}	
+	}	
+}
